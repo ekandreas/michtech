c80838ee27fe04305e0168d88578695f84477e1d
Andreas Ek <andreas@elseif.se>
2017-07-30 15:44:27 +0200

    force https in prod

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@
 */
 
 if (env('APP_ENV') === 'production') {
-    \Illuminate\Support\Facades\URL::forceSchema('https');
+    URL::forceSchema('https');
 }
 
 Route::get('folder/{id}/item/{item}', 'FolderController@download');
