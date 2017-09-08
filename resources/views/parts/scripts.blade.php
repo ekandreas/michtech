0c71572afeb27a17b255eaef340eab458c780d6b
Andreas Ek <andreas@elseif.se>
2017-09-08 09:41:01 +0200

    Adding Sentry

--- resources/views/parts/scripts.blade.php
+++ resources/views/parts/scripts.blade.php
@@ -1,2 +1,6 @@
 <script async type="text/javascript" src="{{ mix('dist/app.js') }}"></script>
 @yield('scripts')
+<script src="https://cdn.ravenjs.com/3.17.0/raven.min.js" crossorigin="anonymous"></script>
+<script>
+    Raven.config('https://475631a7024f44eaa406c96861f144c7@sentry.io/214383').install()
+</script>
